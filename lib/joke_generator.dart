a8aea17f9a5ebed46383b187492410918a2316c0
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-06-22 13:17:36 +0300

    created the function to fetch the joke

---
+++ lib/joke_generator.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<String> fetchJoke({required String category}) async {
+  final response = await http.get(
+    Uri.parse('https://v2.jokeapi.dev/joke/$category?format=txt'),
+  );
+
+  if (response.statusCode == 200) {
+    return response.body;
+  }
+
+  throw Exception("Failed to load joke");
+}
