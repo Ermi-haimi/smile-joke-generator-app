d6bd694b96efd1bc3ac648ad42689474ee08dde4
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-06-23 19:11:04 +0300

    made the favorite jokes data persistent using shared preferences

--- lib/fav_joke_provider.dart
+++ lib/fav_joke_provider.dart
@@ -1,23 +1,43 @@
 import 'dart:collection';
-
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FavJokeProvider extends ChangeNotifier {
-  final List<String> _favJokes = [];
+  List<String> _favJokes = [];
+
+  FavJokeProvider() {
+    loadFavoriteJokes();
+  }
 
   UnmodifiableListView<String> get favJokes => UnmodifiableListView(_favJokes);
 
-  void toggleFavorite({required String joke}) {
+  Future<void> toggleFavorite({required String joke}) async {
     if (_favJokes.contains(joke)) {
       _favJokes.remove(joke);
+      await saveJokes();
     } else {
       _favJokes.add(joke);
+      await saveJokes();
     }
 
     notifyListeners();
   }
 
+  Future<void> saveJokes() async {
+    final sharedPref = await SharedPreferences.getInstance();
+    await sharedPref.setStringList(
+      'favoriteJokes',
+      _favJokes,
+    );
+  }
+
   bool isFavorite(String joke) {
     return _favJokes.contains(joke);
+  }
+
+  Future<void> loadFavoriteJokes() async {
+    final sharedPref = await SharedPreferences.getInstance();
+
+    _favJokes = sharedPref.getStringList('favoriteJokes') ?? [];
   }
 }
